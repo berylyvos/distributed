33ed3a12eaeb9813d33b17e2000e07e6cc09995f
gnix <870656738@qq.com>
2022-11-02 18:31:18 +0800

    init

---
+++ registry/client.go
@@ -0,0 +1,126 @@
+package registry
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+func RegisterService(r Registration) error {
+	heartbeatURL, err := url.Parse(r.HeartbeatURL)
+	if err != nil {
+		return err
+	}
+	http.HandleFunc(heartbeatURL.Path, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	serviceUpdateURL, err := url.Parse(r.ServiceUpdateURL)
+	if err != nil {
+		return err
+	}
+	http.Handle(serviceUpdateURL.Path, &serviceUpdateHandler{})
+
+	buf := new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	err = enc.Encode(r)
+	if err != nil {
+		return err
+	}
+
+	res, err := http.Post(ServicesURL, "application/json", buf)
+	if err != nil {
+		return err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to register service. Registry service responded with code %v", res.StatusCode)
+	}
+
+	return nil
+}
+
+type serviceUpdateHandler struct{}
+
+func (suh serviceUpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	dec := json.NewDecoder(r.Body)
+	var p patch
+	err := dec.Decode(&p)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("updated received %v\n", p)
+	prov.Update(p)
+}
+
+func ShutdownService(url string) error {
+	req, err := http.NewRequest(http.MethodDelete, ServicesURL, bytes.NewBuffer([]byte(url)))
+	if err != nil {
+		return err
+	}
+	req.Header.Add("Content-Type", "text/plain")
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to deregister service. registry service responded with code %v", res.StatusCode)
+	}
+	return nil
+}
+
+type providers struct {
+	services map[ServiceName][]string
+	mutex    *sync.RWMutex
+}
+
+func (p *providers) Update(pat patch) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	for _, entry := range pat.Added {
+		if _, ok := p.services[entry.Name]; !ok {
+			p.services[entry.Name] = make([]string, 0)
+		}
+		p.services[entry.Name] = append(p.services[entry.Name], entry.URL)
+	}
+
+	for _, entry := range pat.Removed {
+		if urls, ok := p.services[entry.Name]; ok {
+			for i := range urls {
+				if urls[i] == entry.URL {
+					p.services[entry.Name] = append(urls[:i], urls[i+1:]...)
+				}
+			}
+		}
+	}
+}
+
+func (p *providers) get(name ServiceName) (string, error) {
+	provURLs, ok := p.services[name]
+	if !ok {
+		return "", fmt.Errorf("no providers available for service %v", name)
+	}
+	idx := int(rand.Float32() * float32(len(provURLs)))
+	return provURLs[idx], nil
+}
+
+func GetProvider(name ServiceName) (string, error) {
+	return prov.get(name)
+}
+
+var prov = providers{
+	services: make(map[ServiceName][]string),
+	mutex:    new(sync.RWMutex),
+}
